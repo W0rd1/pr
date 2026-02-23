1d88ce695d7ad0d1cd7ecad7789a9490ceead2be
Ward khaled <wardkh0zs@gmail.com>
2026-02-23 09:51:27 +0100

    i didn't make any changes

--- src/app/services/product.ts
+++ src/app/services/product.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class Product {
-  
+
 }
